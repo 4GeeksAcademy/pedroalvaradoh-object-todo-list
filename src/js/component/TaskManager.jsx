d0194b5e6cc98d14d5cc31d0affadd64152b3e37
Pedro Luis Alvarado Herrera <129191234+pedroalvaradoh@users.noreply.github.com>
2023-06-16 00:36:45 +0000

    style: Styles added

--- src/js/component/TaskManager.jsx
+++ src/js/component/TaskManager.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import ReactMarkdown from 'react-markdown';
+
 
 
 // Componente de To-do List
@@ -9,11 +11,11 @@ const TaskManager = () => {
     const [taskList, setTaskList] = useState([])
 
     const newTask = () => {
-        if (!titleInputValue && !descriptionInputValue) return;
+        if (!titleInputValue) return;
 
         const newTaskItem = {
             title: titleInputValue,
-            description: descriptionInputValue,
+            description: descriptionInputValue.replace(/\n/g, "<br>"),
         };
 
         setTaskList((prev) => [...prev, newTaskItem]);
@@ -29,27 +31,19 @@ const TaskManager = () => {
 
     const onChangeDescription = (e) => {
         setDescriptionInputValue(e.target.value)
-    }
+    };
 
     // Delete Button
     const handleDelete = (mapIndex) => {
         setTaskList((prev) => taskList.filter((_, index) => index !== mapIndex))
-    }
-    // hacer submit al hacer enter
-    // const handleKeyDown = (e) => {
-    //     if (e.keyCode === 13) {
-    //         e.preventDefault(); // Evita que se agregue una nueva línea en el textarea
-    //         newTask();
-    //     }
-    // };
+    };
 
+    // hacer submit al hacer enter
     const handleKeyDown = (e) => {
         if (e.key === "Enter" && e.shiftKey) {
             setDescriptionInputValue((prevValue) => prevValue + "\n");
             e.preventDefault(); // Evita que se realice el salto de línea predeterminado
-        }
-
-        if (e.key === "Enter") {
+        } else if (e.key === "Enter") {
             e.preventDefault(); // Evita que se agregue una nueva línea en el textarea
             newTask();
         }
@@ -58,18 +52,21 @@ const TaskManager = () => {
 
     return <>
         <div className="general-wraper flex-column container ">
+            <header className="mb-5"><h1>MiTask</h1></header>
             <div className="inputs-class">
-                <input className="input-title mb-2" type="text" placeholder="Task Title" value={titleInputValue} onChange={onChangeTitle} />
-                <textarea className="input-description" placeholder="Describe your task (optional)" value={descriptionInputValue} onChange={onChangeDescription} onKeyDown={handleKeyDown} ></textarea>
+                <input className="input-title mb-2" type="text" placeholder="Task Title" value={titleInputValue} onChange={onChangeTitle} onKeyDown={handleKeyDown} />
+                <textarea className="input-description" placeholder="Describe your task (optional)" value={descriptionInputValue} onChange={onChangeDescription} onKeyDown={handleKeyDown}></textarea>
             </div>
             <button className="add-button" type="submit" onClick={newTask}>Add</button>
             {taskList.length > 0 &&
                 <div className="list-body">
                     <ul className="general-list">
                         {taskList.map((task, index) => (
-                            <li key={index}> <div className="d-flex justify-content-between align-items-center"><h5 className="text-center">{task.title}</h5> <button className="delete-button" type="button" onClick={() => handleDelete(index)}>X</button></div> <div>{task.description}</div> </li>
+                            <li key={index}> <div className="d-flex justify-content-between align-items-center p-2"><h5 className="text-center">- {task.title}</h5> <button className="delete-button" type="button" onClick={() => handleDelete(index)}>x</button></div> <div dangerouslySetInnerHTML={{ __html: task.description }}></div> </li>
                         ))}
+                        <div className="task-counter"> {taskList.length === 0 ? "No Pending Tasks" : `${taskList.length} Pending Tasks`}</div>
                     </ul>
+                    
                 </div>
             }
         </div>
